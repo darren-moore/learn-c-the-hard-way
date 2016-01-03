06c0221646d752b86492d642a02a8b70fbe384e2
Darren Moore <moo.darren@gmail.com>
2016-01-03 10:44:52 -0500

    Finished ex1 EC

--- ex1.c
+++ ex1.c
@@ -1,5 +1,10 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[]){
 	puts("Hello world.");
-
+	puts("Here is some more text");
+	puts("The story is:");
+	puts("The dog sat,");
+	puts("then she met Matt.");
 	return 0;
 }
