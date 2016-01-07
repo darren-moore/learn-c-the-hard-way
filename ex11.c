25f5ad813be86971a8627f8abd64405ef3b021dc
Darren Moore <moo.darren@gmail.com>
2016-01-06 23:32:30 -0500

    Finished several

---
+++ ex11.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	// go through each string in argv
+	int i = 0;
+	while(i < argc) {
+		printf("arg %d: %s\n", i, argv[i]);
+		i++;
+	}
+
+	// Let's make our own array of strings
+	char *states[] = {
+		"California", "Oregon",
+		"Washington", "Texas"
+	};
+	
+	int num_states = 4;
+	i = 0; // watch for this
+	while(i < num_states) {
+		printf("state %d: %s\n", i, states[i]);
+		i++;
+	}
+	
+	// copy into states if enough room
+	i = 0;
+	while(i < num_states && argc <= num_states) {
+		states[i] = argv[i];
+		i++;
+	}
+	
+	i = 0; // watch for this
+	while(i < num_states) {
+		printf("state %d: %s\n", i, states[i]);
+		i++;
+	}
+	return 0;
+}
